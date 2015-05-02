1fcfe0a4eeb58772837e79b5e049f8f70cde9bed
sushmithapunati <sushmithapunati@gmail.com>
2015-05-03 03:43:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sree/myapplication/TaskListActivity.java
@@ -0,0 +1,7 @@
+package com.example.sree.myapplication;
+
+/**
+ * Created by sree on 4/28/2015.
+ */
+public class TaskListActivity {
+}
---
+++ app/src/main/java/com/example/sree/myapplication/ToDoList.java
@@ -0,0 +1,113 @@
+package com.example.sree.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+import android.widget.ListAdapter;
+import com.example.sree.myapplication.TaskContract;
+import com.example.sree.myapplication.TaskDBHelper;
+
+
+public class ToDoList extends ListActivity {
+
+    private ListAdapter listAdapter;
+    private TaskDBHelper helper;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_list);
+        updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add a task");
+                builder.setMessage("What do you want to do?");
+                final EditText inputField = new EditText(this);
+                builder.setView(inputField);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String task = inputField.getText().toString();
+
+                        helper = new TaskDBHelper(ToDoList.this);
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+
+                        values.clear();
+                        values.put(TaskContract.Columns.TASK,task);
+
+                        db.insertWithOnConflict(TaskContract.TABLE,null,values,SQLiteDatabase.CONFLICT_IGNORE);
+                        updateUI();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel",null);
+
+                builder.create().show();
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    private void updateUI() {
+        helper = new TaskDBHelper(ToDoList.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+                new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK},
+                null, null, null, null, null);
+
+        listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.task_view,
+                cursor,
+                new String[]{TaskContract.Columns.TASK},
+                new int[]{R.id.taskTextView},
+                0
+        );
+
+        this.setListAdapter(listAdapter);
+    }
+
+    public void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView taskTextView = (TextView) v.findViewById(R.id.taskTextView);
+        String task = taskTextView.getText().toString();
+
+        String sql = String.format("DELETE FROM %s WHERE %s = '%s'",
+                TaskContract.TABLE,
+                TaskContract.Columns.TASK,
+                task);
+
+
+        helper = new TaskDBHelper(ToDoList.this);
+        SQLiteDatabase sqlDB = helper.getWritableDatabase();
+        sqlDB.execSQL(sql);
+        updateUI();
+    }
+}
